6175016f4003589d28299b2a4b1a4bf90d2b3030
Yulia <yulia.scherbina@gmail.com>
2018-04-04 17:56:01 +0200

    basic usage guide, form reducer

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # redux-form
 
-##
-reducer - 
+## Transaltions
+reducer - преобразователь
 
 ## Overview
 To connect your React form components to your Redux store you'll need the following pieces from the redux-form package:
--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
+const rootReducer = combineReducers({
+    // ...your other reducers here
+    // you have to pass formReducer under 'form' key,
+    // for custom keys look up the docs for 'getFormState'
+    form: formReducer
+})
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
